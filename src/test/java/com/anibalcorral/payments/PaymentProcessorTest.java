15ff8328c6f6832adc603723b41ea5e842c730d1
Anibal Corral Serrano <acorral@kdu.cl>
2024-11-06 23:56:42 -0300

    Refactoring of tests and use before

--- src/test/java/com/anibalcorral/payments/PaymentProcessorTest.java
+++ src/test/java/com/anibalcorral/payments/PaymentProcessorTest.java
@@ -1,5 +1,6 @@
 package com.anibalcorral.payments;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -11,18 +12,36 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class PaymentProcessorTest {
 
+private PaymentGateway pg;
+private PaymentProcessor pp;
+@BeforeEach
+public void setup(){
+     pg = Mockito.mock(PaymentGateway.class);
+     pp = new PaymentProcessor(pg);
+
+}
+    /**
+     * Partes de un test:
+     * 1 - Preparación del Escenario: se preparan los objetos.
+     * 2 - Llamada al metodo que quiero probar.
+     * 3 - Comprobacion del resultado que estoy esperando.
+     */
     @Test
     public void payment_is_correct(){
-        PaymentGateway pg = Mockito.mock(PaymentGateway.class);
+        //Se preparan los objetos que quiero usar en el test
+        //PaymentGateway pg = Mockito.mock(PaymentGateway.class);
         Mockito.when(pg.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-        PaymentProcessor pp = new PaymentProcessor(pg);
-        assertTrue(pp.makePayment(1000));
+        //PaymentProcessor pp = new PaymentProcessor(pg);
+        // Lo ququiero probar
+        boolean result = pp.makePayment(1000);
+        // Comprobación del resultado
+        assertTrue(result);
     }
     @Test
     public void payment_is_incorrect(){
-        PaymentGateway pg = Mockito.mock(PaymentGateway.class);
+        //PaymentGateway pg = Mockito.mock(PaymentGateway.class);
         Mockito.when(pg.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-        PaymentProcessor pp = new PaymentProcessor(pg);
+        //PaymentProcessor pp = new PaymentProcessor(pg);
         assertFalse(pp.makePayment(1000));
     }
 }
